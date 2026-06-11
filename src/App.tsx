f5f1aca1df49087fb548b09345bc39376cbf742f
Sayed161 <sheikhsayed23@gmail.com>
2026-06-11 22:23:22 +0600

    Use BrowserRouter + add render.yaml SPA rewrite for Render

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { CartProvider } from './context/CartContext';
